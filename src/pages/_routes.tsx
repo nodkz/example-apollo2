cca551834c7bd3ccedf81be2a6a339628153f470
nodkz <pavel.chertorogov@gmail.com>
2020-02-12 12:33:53 +0600

    refactor: migrate on ApolloHooks

--- src/pages/_routes.tsx
+++ src/pages/_routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Index from './';
+import { Index } from './';
 import Error from './_error';
-import Orders from './orders/OrderQuery';
+import { OrderQuery } from './orders/OrderQuery';
 
 export default function AppRoute() {
   return (
     <Switch>
       <Route path="/" exact component={Index} />
-      <Route path="/orders" component={Orders} />
+      <Route path="/orders" component={OrderQuery} />
       {/* <Route path="/products" component={Products} /> */}
       {/* <Route path="/customers" components={Customers} /> */}
       <Route component={Error} />
--- src/pages/orders/OrderQuery.tsx
+++ src/pages/orders/OrderQuery.tsx
@@ -1,48 +1,38 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import qs from 'qs';
-import { OrderListQueryComponent } from './__generated__/OrderQuery';
-import OrderList from './OrderList';
+import { useOrderListQuery } from './__generated__/OrderQuery';
+import { OrderList } from './OrderList';
 
 interface Props extends RouteComponentProps {}
 
-export default class OrderQuery extends React.Component<Props> {
-  setPage = (page: number) => {
-    const { search } = this.props.location;
+export function OrderQuery(props: Props) {
+  const setPage = (page: number) => {
+    const { search } = props.location;
     const query = qs.parse(search, { ignoreQueryPrefix: true }) || {};
 
-    this.props.history.push({
+    props.history.push({
       search: qs.stringify({ ...query, page }),
     });
   };
 
-  render() {
-    const { search } = this.props.location;
-    const query = qs.parse(search, { ignoreQueryPrefix: true }) || {};
-    const page = parseInt(query.page, 10) || 1;
-    const perPage = parseInt(query.perPage, 10) || 10;
+  const { search } = props.location;
+  const query = qs.parse(search, { ignoreQueryPrefix: true }) || {};
+  const page = parseInt(query.page, 10) || 1;
+  const perPage = parseInt(query.perPage, 10) || 10;
 
-    return (
-      <div>
-        <OrderListQueryComponent
-          variables={{
-            page,
-            perPage,
-          }}
-        >
-          {({ error, loading, data }) => {
-            if (loading) return <div>Loading...</div>;
-            if (error) return <div>Error: {error.message}</div>;
-            if (data && data.viewer && data.viewer.orderPagination) {
-              return (
-                <OrderList pagination={data.viewer.orderPagination} onSetPage={this.setPage} />
-              );
-            }
+  const { error, loading, data } = useOrderListQuery({
+    variables: {
+      page,
+      perPage,
+    },
+  });
 
-            return null;
-          }}
-        </OrderListQueryComponent>
-      </div>
-    );
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (data && data.viewer && data.viewer.orderPagination) {
+    return <OrderList pagination={data.viewer.orderPagination} onSetPage={setPage} />;
   }
+
+  return null;
 }
